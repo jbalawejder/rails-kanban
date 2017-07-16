c33b5a3b5723efb6c5e314f6a0ec7bce30fca177
josephbalawejder <joseph@influitive.com>
2017-07-16 11:22:55 -0400

    Added functionality of tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714200808) do
+ActiveRecord::Schema.define(version: 20170714204229) do
 
   create_table "tasks", force: true do |t|
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "state",      default: "open"
+    t.integer  "user_id"
   end
 
 end
